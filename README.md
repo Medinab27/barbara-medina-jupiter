7dad7fe8e3484e741f2f309316407a6572e280b6
Barbara Medina <BarbaraM@mckinstry.com>
2025-02-05 09:57:04 -0800

    API fetch repository, in the READ.ME file there is a link to the new API project to a different repository

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Portfolio Project for Intro to Programming course with Code the Dream
 
 Barbara Medina
+
+ [My Open API Project](https://github.com/Medinab27/barbaram-jupiter-open-api)
--- js folder/index.js
+++ js folder/index.js
@@ -7,6 +7,8 @@ const messageForm = document.querySelector("[name='leave_message']");
 const messageSection = document.querySelector("#messages");
 const messageList = messageSection.querySelector("ul");
 const removeButton = document.createElement("button");
+const projectSection = document.querySelector("#Projects"); //refers to Projects section by id
+const projectsList = projectSection.querySelector("ul"); //assigning a projectsList name to the referenced ul element
 
 const skills = ["Javacript", "HTML", "CSS", "GitHub", "Revit", "BIM360", "IES"];
 
@@ -64,3 +66,24 @@ messageForm.addEventListener("submit", (event) => {
   //Resets the form after the submit button has been clicked
   messageForm.reset();
 });
+
+//fetch for populating Projects section of portfolio
+fetch("https://api.github.com/users/Medinab27/repos")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const repositories = data;
+    console.log(repositories);
+
+    for (let i = 0; i < repositories.length; i++) {
+      const project = repositories[i].name; // assign to a variable project the name portion of the i array.
+      //create an <li> element in the HTML document using DOM
+      const li = document.createElement("li");
+      li.innerText = project; //assign the name of the project to the li element 
+      projectsList.appendChild(li); //add the li element with the project name to the HTML through the projectsList called in up above
+    }
+  })
+  .catch((error) => {
+    console.error("An error occured", error);
+  });
